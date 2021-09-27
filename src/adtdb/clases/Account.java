00dcc3250ccb9f1e662e5e76f1ae6e87afd8a2aa
Bot-Steven <83646314+Bot-Steven@users.noreply.github.com>
2021-09-27 12:31:28 +0200

    Primer commit con los packages info y algunas entidades

---
+++ src/adtdb/clases/Account.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adtdb.clases;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Account implements java.io.Serializable{
+    
+    private long id;
+    private String description;
+    private float balance,creditLine,beginBalance;
+    private Timestamp beginBalanceTimestamp;
+    private AccountType type;
+    
+    public Account() {
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public float getCreditLine() {
+        return creditLine;
+    }
+
+    public void setCreditLine(float creditLine) {
+        this.creditLine = creditLine;
+    }
+
+    public float getBeginBalance() {
+        return beginBalance;
+    }
+
+    public void setBeginBalance(float beginBalance) {
+        this.beginBalance = beginBalance;
+    }
+
+    public Timestamp getBeginBalanceTimestamp() {
+        return beginBalanceTimestamp;
+    }
+
+    public void setBeginBalanceTimestamp(Timestamp beginBalanceTimestamp) {
+        this.beginBalanceTimestamp = beginBalanceTimestamp;
+    }
+
+    public AccountType getType() {
+        return type;
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
+
+    
+    
+}
---
+++ src/adtdb/clases/Customer.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adtdb.clases;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Customer implements java.io.Serializable{
+    
+    private int zip;
+    private long id,phone;
+    private String firstName,lastName,middleInitial,street,city,state,email;
+
+    public Customer() {  
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
